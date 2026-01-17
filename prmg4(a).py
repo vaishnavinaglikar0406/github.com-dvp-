29185eaa455e04f824aaec38173cbde357d53014
Vaishnavi Naglikar <vaishnavinaglikar0406@gmail.com>
2026-01-17 16:27:53 +0530

    prmg4(a).py

---
+++ prmg4(a).py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt 
+categories = ['Category A', 'Category B', 'Category C', 'Category D'] 
+values = [10, 15, 7, 12] 
+plt.bar(categories, values, color='green') 
+plt.xlabel('Categories') 
+plt.ylabel('Values') 
+plt.title('Bar Plot Example') 
+plt.show() 
