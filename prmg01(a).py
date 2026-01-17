7822a2d6a2a756a3949a6bb93967b3a215639255
Vaishnavi Naglikar <vaishnavinaglikar0406@gmail.com>
2026-01-17 17:01:15 +0530

    prmg01(a).py

--- prmg01(a).py
+++ prmg01(a).py
@@ -7,8 +7,8 @@ print("Marks2:", m2)
 print("Marks3:", m3)  
 small=m1 
 if small > m2: 
-small = m2 
+  small = m2 
 if small > m3: 
-small = m3 
+  small = m3 
 avg=(m1+m2+m3-small)/2; 
 print("Average of best two test out of three test is",avg)
