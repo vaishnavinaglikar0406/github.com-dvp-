bf80712d1190dca14f6195f05dced3931a6be05d
Vaishnavi Naglikar <vaishnavinaglikar0406@gmail.com>
2026-01-18 13:32:13 +0530

    prmg3(a).py

--- prmg3(a).py
+++ prmg3(a).py
@@ -3,13 +3,13 @@ word_count = len(sentence.split())
 digit_count = 0 
 upper_count = 0 
 lower_count = 0 
-for char in sentence: 
-if char.isdigit(): 
-digit_count += 1 
-elif char.isupper(): 
-upper_count += 1 
-elif char.islower(): 
-lower_count += 1 
+for char in sentence:
+  if char.isdigit(): 
+    digit_count += 1 
+  elif char.isupper(): 
+    upper_count += 1 
+  elif char.islower(): 
+    lower_count += 1 
 print("Number of words:", word_count) 
 print("Number of digits:", digit_count) 
 print("Number of uppercase letters:", upper_count) 
