4c2deed411be12d7558c647dba621da59ce0d797
Vaishnavi Naglikar <vaishnavinaglikar0406@gmail.com>
2026-01-18 13:30:35 +0530

    prmg2(b).py

--- prmg2(b).py
+++ prmg2(b).py
@@ -1,25 +1,25 @@
 def binary_to_decimal(binary): 
-decimal = 0 
-power = 0 
-while binary > 0: 
-decimal += (binary % 10) * (2 ** power) 
-binary //= 10 
-power += 1 
-return decimal 
+  decimal = 0 
+  power = 0 
+  while binary > 0: 
+    decimal += (binary % 10) * (2 ** power) 
+    binary //= 10
+    power += 1 
+    return decimal 
 def octal_to_hexadecimal(octal): 
-decimal = 0 
-power = 0 
-while octal > 0: 
-decimal += (octal % 10) * (8 ** power) 
-octal //= 10 
-power += 1 
-hexadecimal = "" 
-hex_digits = "0123456789ABCDEF" 
-while decimal > 0: 
-remainder = decimal % 16 
-hexadecimal = hex_digits[remainder] + hexadecimal 
-decimal //= 16 
-return hexadecimal 
+  decimal = 0 
+  power = 0 
+  while octal > 0: 
+    decimal += (octal % 10) * (8 ** power) 
+    octal //= 10 
+    power += 1 
+  hexadecimal = "" 
+  hex_digits = "0123456789ABCDEF" 
+  while decimal > 0: 
+    remainder = decimal % 16 
+    hexadecimal = hex_digits[remainder] + hexadecimal 
+    decimal //= 16 
+  return hexadecimal 
 binary_number = int(input("Enter a binary number: ")) 
 decimal_number = binary_to_decimal(binary_number) 
 print("Decimal:", decimal_number) 
