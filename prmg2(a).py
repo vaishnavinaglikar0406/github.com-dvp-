1f2abf3ea28b83ae50046c95f5fcf19e09705851
Vaishnavi Naglikar <vaishnavinaglikar0406@gmail.com>
2026-01-18 13:26:06 +0530

    prmg2(a).py

--- prmg2(a).py
+++ prmg2(a).py
@@ -1,14 +1,14 @@
-def fibonacci(n): 
-if n <= 0: 
-print("Error: N must be greater than 0.") 
-return None 
-elif n == 1: 
-return 0 
-elif n == 2: 
-return 1 
-else: 
-return fibonacci(n-1) + fibonacci(n-2) 
-n = int(input("Enter a value for N: ")) 
+def fibonacci(n):
+  if n <= 0: 
+    print("Error: N must be greater than 0.") 
+    return None
+  elif n == 1: 
+    return 0 
+  elif n == 2: 
+    return 1 
+  else: 
+    return fibonacci(n-1) + fibonacci(n-2) 
+    n = int(input("Enter a value for N: ")) 
 result = fibonacci(n) 
 if result is not None: 
-print("The", n, "th term of the Fibonacci sequence is", result)
+  print("The", n, "th term of the Fibonacci sequence is", result)
